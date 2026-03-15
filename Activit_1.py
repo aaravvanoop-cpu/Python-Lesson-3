97c1b97bb1f9f04454a7c6591cbfd3c9374763e3
aaravvanoop-cpu <aaravvanoop@gmail.com>
2026-03-15 18:33:43 +0000

    Add files via upload

---
+++ Activit_1.py
@@ -0,0 +1,25 @@
+# Assigning Different Variables
+name = "Aarav"
+age = 10
+is_student = True
+Weight = 38.5
+
+# Printing Different Variables and their Data Type
+print("Name :",name)
+print("Data Type of Name is",type(name))
+
+print("Age :",age)
+print("Age :",type(age))
+
+print("is_student :",is_student)
+print("is_student :",type(is_student))
+
+print("Wheight :",Weight)
+print("Weight :",type(Weight))
+
+age = str(age)
+print(age)
+print("Data type of age is",type(age))
+Weight = int(Weight)
+print(Weight)
+print("Data Type of Weight is",type(Weight))
